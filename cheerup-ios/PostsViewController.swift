f24a1450e95d2090285844340c9d2a76adb1e40f
dannertech <danner@danner.tech>
2019-01-23 01:03:20 -0500

    updated posts view controller

--- cheerup-ios/PostsViewController.swift
+++ cheerup-ios/PostsViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import Firebase
-
+import SDWebImage
 
 
 class PostsViewController: UIViewController {
+    var postDictionary : NSDictionary?
+    var currentPost : String?
+    var postData = [Cheerup]()
     
     @IBOutlet var postsImage: UIImageView!
     
@@ -25,34 +28,100 @@ class PostsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let storage = Storage.storage()
-        
-    let storageRef = storage.reference()
-        // Do any additional setup after loading the view.
-            let gsReference = Storage.storage().reference(forURL: "https://firebasestorage.googleapis.com/v0/b/cheerup-326b3.appspot.com/o/posts?alt=media&token=391e396a-749c-412b-9e13-a6752991028e")
-        
-        gsReference.getData(maxSize: 1 * 1024 * 1024) { data, error in
-            if let error = error {
-                print(error)
-            } else {
-                let image = UIImage(data: data!)
-                self.postsImage.image = image
+        let postsRef = Database.database().reference().child("posts")
+        postsRef.observeSingleEvent(of: .value, with: {
+            (snapshot) in
+            //        guard let value = snapshot.value as? [String:Any?] else { return }
+            //       guard let json = value["posts"] as? [String:Any?] else { return }
+            let allPosts = snapshot.children.allObjects as! [DataSnapshot]
+            
+            for postSnap in allPosts {
+                let postKey = postSnap.key
+                let postText = postSnap.childSnapshot(forPath: "post").value as! String
+                let photoURL = postSnap.childSnapshot(forPath: "photoURL").value as! String
+                let aPost = Cheerup(withKey: postKey, andPost: postText, andPhoto: photoURL)
+                self.postData.append(aPost)
+                
             }
+            self.postsTable.reloadData()
+            
+            
+            
+        }) { (error) in
+            print(error.localizedDescription)
         }
+        
+        
+        
+        
+        
+        postsTable.dataSource = self
+        postsTable.delegate = self
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+        // let gsReference = Storage.storage().reference(forURL: "https://firebasestorage.googleapis.com/v0/b/cheerup-326b3.appspot.com/o/posts?alt=media&token=391e396a-749c-412b-9e13-a6752991028e")
+        
+        /*      gsReference.getData(maxSize: 1 * 1024 * 1024) { data, error in
+         /         if let error = error {
+         print(error)
+         } else {
+         let image = UIImage(data: data!)
+         self.postsImage.image = image
+         }
+         }
+         */
+        
+        
+        
     }
-
-
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func logOut(_ sender: Any) {
+        
+        let firebase = Auth.auth()
+        do {
+            try firebase.signOut()
+        } catch let signOutError as NSError {
+            print(signOutError)
+        }
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        self.present(loginViewController, animated: true, completion: nil)
+        
     }
-    */
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return self.postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = self.postsTable.dequeueReusableCell(withIdentifier: "cell") as! CustomTableViewCell
+        //  cell.postText.text = String(indexPath.row)
+        let data = postData[indexPath.row].post
+        let imageURLString = postData[indexPath.row].photoURL
+        let imageURL = NSURL(string: imageURLString)
+        
+        print(data)
+        cell.postText.text = data
+        let placeholderImage = UIImage(named: "purple-field")
+        let imageView = cell.postImage
+        imageView?.sd_setImage(with: imageURL as! URL, placeholderImage: placeholderImage)
+        
+        print(imageURL)
+        return cell
+    }
 }
